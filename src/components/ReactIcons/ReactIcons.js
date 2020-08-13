c2971c8427e2238c2ea934c6e9c8535d86a52f23
mosesbenjamin <60680552+El-Mavewrick@users.noreply.github.com>
2020-08-13 07:46:37 +0100

    toast

--- src/components/ReactIcons/ReactIcons.js
+++ src/components/ReactIcons/ReactIcons.js
@@ -10,7 +10,7 @@ const ReactIcons = () => {
     return (
         <>
         <div className="icon-container mb-5">
-            <h4>React Icons</h4>
+            <h4 className="mb-3">React Icons</h4>
             <IconContext.Provider value={{ color: "blue", size:"5rem" }}>
                 {/* <FaReact color="purple" size="10rem"/>
                 <MdAlarm color="green" size="10rem"/> */}
--- src/components/ReactToastify/ReactToastify.js
+++ src/components/ReactToastify/ReactToastify.js
@@ -4,15 +4,34 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './ReactToastify.css';
 
+
+
+//Custom Toast Component
+const CustomToast = ({ closeToast })=>{
+    return(
+        <div>
+            Something went wrong!
+            <button  className="btn btn-sm btn-secondary ml-3" onClick={closeToast}>Close</button>
+        </div>
+    );
+}
+
 toast.configure();
 const ReactToastify = () => {
 
     const notify = () =>{
+        // Autoclose deault is 5s
         toast("Basic notification", {position: toast.POSITION.TOP_LEFT})
-        toast("Basic notification", {position: toast.POSITION.TOP_CENTER})
-        toast("Basic notification", {position: toast.POSITION.TOP_RIGHT})
-        toast("Basic notification", {position: toast.POSITION.BOTTOM_LEFT})
-        toast("Basic notification", {position: toast.POSITION.BOTTOM_CENTER})
+        toast.success("Basic notification", {
+            position: toast.POSITION.TOP_CENTER,
+            autoClose: 8000
+        })
+        toast.info("Basic notification", {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: false
+        })
+        toast.warn(<CustomToast />, {position: toast.POSITION.BOTTOM_LEFT})
+        toast.error("Basic notification", {position: toast.POSITION.BOTTOM_CENTER})
         toast("Basic notification", {position: toast.POSITION.BOTTOM_RIGHT})
     }
 
@@ -20,7 +39,7 @@ const ReactToastify = () => {
         <>
         <div className="toast-container">
             <h4>Toast Notifications</h4>
-            <button className="btn btn-primary" onClick={notify}>Notify</button>
+            <button className="btn btn-primary mt-3" onClick={notify}>Notify</button>
         </div>
         </>
     );
